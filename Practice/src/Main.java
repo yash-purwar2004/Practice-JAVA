2e171312d6e38219cd2aa66e9152026ca47ae484
yash-sans2004 <nayan251198@gmail.com>
2023-08-23 00:03:26 +0530

    Basic code of Java

---
+++ Practice/src/Main.java
@@ -0,0 +1,35 @@
+public class Main{
+    public static void main(String[] args){
+        // Code for printing a hello world on screen
+//        System.out.println("Hello World");
+//        System.out.print("Hello Yash");
+
+
+    // Variables
+//        String name = "Yash";
+//        int age = 30;
+//        String cpy = name;
+//
+//        System.out.println(age);
+
+
+//    byte - 1[-128 to 127]
+//    short 2;
+//    int - 4,1,2,3;
+//    long - 8
+//    float - 4 3.14
+//    double - 8
+//    char - 2 a,b,c,d
+//    boolean - 1 true/false
+
+
+//      byte age = 12;
+//      int phone = 123456789;
+//      long phone2 = 123456789000L;
+//      float pi = 3.14F;
+//      char letter = '@';
+//      boolean isAdult = false;
+
+
+    }
+}
